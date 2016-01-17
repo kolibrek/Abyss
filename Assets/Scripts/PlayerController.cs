ef8585f97b4db5aeddd42e82c8654e1db12badbf
Kevin Clemons <kolibermedia@gmail.com>
2016-01-17 00:21:53 -0700

    Adding all Unity project files as well as initial scripts.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Controller2D))]
+public class PlayerController : MonoBehaviour {
+
+	public float jumpHeight = 4f;
+	public float timeToJumpApex = .5f;
+	float accelerationTimeAirborne = 0.3f;
+	float accelerationTimeGrounded = 0.1f;
+	public float moveSpeed = 4f;
+
+	float gravity;
+	float jumpVelocity;
+	Vector3 velocity;
+	float velocityXSmoothing;
+
+	private GameObject player;
+	private Controller2D controller;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<Controller2D> ();
+		player = GameObject.Find ("Player");
+
+		gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex,2);
+		jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+
+		print ("Gravity: " + gravity + "Jump Velocity: " + jumpVelocity);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (controller.collisions.above || controller.collisions.below) {
+			velocity.y = 0;
+		}
+
+		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		if (Input.GetKeyDown(KeyCode.Space) && controller.collisions.below) {
+			velocity.y = jumpVelocity;
+		}
+
+		float targetVelocityX = input.x * moveSpeed;
+		velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
+		velocity.y += gravity * Time.deltaTime;
+		controller.Move(velocity * Time.deltaTime);
+	}
+
+	void FixedUpdate () {
+
+	}
+}
