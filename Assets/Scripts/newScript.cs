6fbfaf3c280c84d5a3b721ae8da4ddef2fb2a1c2
Kevin Clemons <10484164@CS518-03.ad.uvu.edu>
2016-01-21 08:25:03 -0700

    upkeep

--- Assets/Scripts/newScript.cs
+++ Assets/Scripts/newScript.cs
@@ -5,11 +5,16 @@ public class newScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		int x = MultiplyByTwo (75);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	// returns an int that is double the input.
+	int MultiplyByTwo(int num) {
+		return num * 2;
 	}
 }
