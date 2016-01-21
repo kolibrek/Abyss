9477ec1ea140cb68538c626c3c18115e7020ef1a
Kevin Clemons <10484164@CS518-03.ad.uvu.edu>
2016-01-21 09:41:09 -0700

    Messing around in class

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorChanger : MonoBehaviour {
+
+	Color original;
+	Renderer current;
+
+	// Use this for initialization
+	void Start () {
+		current = GetComponent<Renderer> ();
+		original = new Color(0.7f, 0.3f, 0.3f, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.C)) {
+			ChangeColor();
+		}
+		if (Input.GetKeyUp (KeyCode.C)) {
+			RevertColor();
+		}
+	}
+
+	// returns an int that is double the input.
+	void ChangeColor() {
+		current.material.color = new Color (Random.value, Random.value, Random.value, 1);
+	}
+
+	void RevertColor() {
+		current.material.color = original;
+	}
+}
